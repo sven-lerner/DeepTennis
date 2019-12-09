22c9128d2bd0a77e9c099c933dfadf8c4779cf24
Sven Lerner <sven@DN0a2211e6.SUNet>
2019-12-08 17:19:39 -0800

    cleanup loss functions

--- models/loss_functions.py
+++ models/loss_functions.py
@@ -5,27 +5,30 @@ import numpy as np
 import math
 
 def sigmoid(x):
-    #aware this is hidiously inneficient, will improve when training on aws when speed = $$
     return 1 / (1 + math.exp(-x))
 
 def weighted_loss(y_gt, y_pred, device, weighting=None, alpha=.9, #loss_fn=torch.nn.BCELoss(reduction='none')
-    loss_fn=torch.nn.MSELoss(reduction='none')
+    loss_fn=torch.nn.MSELoss(reduction='none'), scaling='linear', smooth=False
     ):
     weight = np.ones(y_gt.shape)
     m = weight.shape[1]
     for i in range(m):
         if weighting is None:
-            weight[:,i] = weight[:,i]*(i/m)
-            # weight[:,i] = weight[:,i]*sigmoid((i - 3*m/4)*10/m)
-            # weight[:,i] = 1 - (1/((99/m)*i + 1))
-            pass
+            if scaling == 'linear':
+                weight[:,i] = weight[:,i]*(i/m)
+            elif scaling == 'sigmoid':
+                weight[:,i] = weight[:,i]*sigmoid((i - 3*m/4)*10/m)
+            elif scaling == 'inverted_exponential_decay':
+                weight[:,i] = 1 - (1/((99/m)*i + 1))
         else:
             weight[i] = weight[i]*weighting
     weight = torch.from_numpy(weight).to(device)
     pointwise_loss = weight * loss_fn(y_pred.unsqueeze(0), y_gt)
-    # assert np.sum(torch.isnan(pointwise_loss).detach().numpy()) < 1, "hit a nan"
 
-    return torch.sum(pointwise_loss) + smooth_loss(y_pred, 3)
+    loss = torch.sum(pointwise_loss) 
+    if smooth:
+        loss += smooth_loss(y_pred, 3)
+    return loss
 
 def smooth_loss(y_pred, weight):
     delta = y_pred[1:] - y_pred[:-1]
@@ -35,8 +38,5 @@ def smooth_loss(y_pred, weight):
 def auto_weighted_loss(y_gt, y_pred, mask, mask_weight, loss_fn=torch.nn.MSELoss(reduction='none')):
     pointwise_loss = torch.sum(mask * loss_fn(y_pred.unsqueeze(0), y_gt))
     explainability_loss = torch.norm(1-mask, p=2) * mask_weight
-    # print(pointwise_loss, explainability_loss)
-    # print(mask)
     loss = explainability_loss + pointwise_loss
-    # print(loss)
     return loss
